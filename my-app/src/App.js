311f7774c42d0f763117ef04dccac3852bfbcaf7
bakhtino <123951428+bakhtino@users.noreply.github.com>
2023-07-22 16:08:19 +0330

    Removed unnecessary files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+
