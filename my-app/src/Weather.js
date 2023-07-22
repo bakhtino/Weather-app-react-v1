8b15e3ed9aa9af68cd9d1334a2d066c3643b8c5f
bakhtino <123951428+bakhtino@users.noreply.github.com>
2023-07-22 19:58:10 +0330

    Remove id from HTML in Weather-react

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -7,38 +7,38 @@ export default function Weather() {
         <body>
     <div className="container">
       <div className="weather-app">
-        <form id="search-form" className="mb-3">
+        <form  className="mb-3">
           <div className="row">
             <div className="col-9">
-              <input type="search" placeholder="Type city name.." className="form-control" id="city-input" autocomplete="off"/>
+              <input type="search" placeholder="Type city name.." className="form-control" autocomplete="off"/>
             </div>
             <div className="col-3">
               <input type="submit" value="Search" className="btn btn-primary w-300"/>
             </div>
           </div>
         </form>
-        <h1 id="city">Tehran</h1>
+        <h1 >Tehran</h1>
         <ul>
-          <li>Last updated: <span id="date">Friday 23:48 </span></li>
+          <li>Last updated: <span >Friday 23:48 </span></li>
           <li id="description" className="description">few clouds</li>
         </ul>
         <div className="row">
           <div className="col-6">
             <div className="clearfix weather-temperature">
-              <img src=" http://openweathermap.org/img/wn/02n@2x.png" alt="few clouds" className="float-left" id="icon"/>
+              <img src=" http://openweathermap.org/img/wn/02n@2x.png" alt="few clouds" className="float-left" />
 
-              <strong id="temperature">34</strong>
+              <strong >34</strong>
               <span className="units">℃ </span>
             </div>
           </div>
           <div className="col-6">
             <ul>
-              <li>Humidity : <span id="humidity">12</span> %</li>
-              <li>Wind : <span id="wind">4</span>Km/h</li>
+              <li>Humidity : <span >12</span> %</li>
+              <li>Wind : <span >4</span>Km/h</li>
             </ul>
           </div>
         </div>
-        <div className="weather-forecast" id="forecast"><div class="row"><div className="col-2">
+        <div className="weather-forecast" ><div class="row"><div className="col-2">
               <div className="weather-forecast-date">Fri
               </div>
               <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="" width="42"/>
