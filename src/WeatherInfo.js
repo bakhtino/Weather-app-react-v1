ab99702c1594eb671bdd28b1d4593babe3034afa
bakhtino <123951428+bakhtino@users.noreply.github.com>
2023-07-27 21:35:03 +0330

    Display the relevant temperature

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
 export default function WeatherInfo(props){
     return (
       <div className="weatherInfo">
@@ -12,13 +14,12 @@ export default function WeatherInfo(props){
 <div className="row">
   <div className="col-6">
     <div className="clearfix weather-temperature">
-      <div >
-
+      <div className="float-left" >
       <WeatherIcon code={props.data.icon}  />
       </div>
-      
-      <strong >{Math.round(props.data.temperature)}</strong>
-      <span className="units">℃ </span>
+      <div className="float-left">
+      <WeatherTemperature celsiuc={props.data.temperature} />
+      </div>    
     </div>
   </div>
   <div className="col-6">
@@ -28,61 +29,7 @@ export default function WeatherInfo(props){
     </ul>
   </div>
 </div>
-<div className="weather-forecast" ><div class="row"><div className="col-2">
-      <div className="weather-forecast-date">Fri
-      </div>
-      <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="" width="42"/>
-      <div className="weather-forecast-temperature">
-      <span classNames="weather-forecast-temperature-max">38° </span> 
-      <span className="weather-forecast-temperature-min">31° </span> 
-      </div>
-    </div><div className="col-2">
-      <div className="weather-forecast-date">Sat
-      </div>
-      <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="" width="42"/>
-      <div className="weather-forecast-temperature">
-      <span className="weather-forecast-temperature-max">39° </span> 
-      <span className="weather-forecast-temperature-min">31° </span> 
-      </div>
-    </div><div className="col-2">
-      <div className="weather-forecast-date">Sun
-      </div>
-      <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="42"/>
-      <div className="weather-forecast-temperature">
-      <span className="weather-forecast-temperature-max">38° </span> 
-      <span className="weather-forecast-temperature-min">31° </span> 
-      </div>
-    </div><div className="col-2">
-      <div className="weather-forecast-date">Mon
-      </div>
-      <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="42"/>
-      <div className="weather-forecast-temperature">
-      <span className="weather-forecast-temperature-max">38° </span> 
-      <span className="weather-forecast-temperature-min">31° </span> 
-      </div>
-    </div><div className="col-2">
-      <div className="weather-forecast-date">Tue
-      </div>
-      <img src="http://openweathermap.org/img/wn/03d@2x.png" alt="" width="42"/>
-      <div className="weather-forecast-temperature">
-      <span className="weather-forecast-temperature-max">39° </span> 
-      <span className="weather-forecast-temperature-min">32° </span> 
-      </div>
-    </div><div className="col-2">
-      <div className="weather-forecast-date">Wed
-      </div>
-      <img src="http://openweathermap.org/img/wn/02d@2x.png" alt=" " width="42"/>
-      <div className="weather-forecast-temperature">
-      <span className="weather-forecast-temperature-max">39° </span> 
-      <span className="weather-forecast-temperature-min">33° </span> 
-      </div>
-    </div></div></div>
-<footer>
-<a href="https://github.com/bakhtino/Vanilla-Weather_app" target="_blank"  rel="noreferrer">Open source code</a>
-by
-<a href="https://www.linkedin.com/in/noushin-bakhtiari-b6314297/?originalSubdomain=ir" target="_blank" rel="noreferrer">Noushin Bakhtiari</a>
-in 7 Feb 2023
-</footer>
+
   </div>
     );
 }
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,38 @@
+import React,{useState} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function WeatherTemperature(props){
+    const [unit , setUnit]= useState("celsiuc");
+    function showFahrenhiet(event){
+        event.preventDefault();
+        setUnit("fahrenhiet");
+    }
+    function showCelsiuc(event){
+        event.preventDefault();
+        setUnit("celsiuc");
+    }
+
+    if (unit=== "celsiuc"){
+        return(
+            <div>
+    <strong >{Math.round(props.celsiuc)}</strong>
+          <span className="units"> ℃  | <a href="/" onClick={showFahrenhiet} className="text-decoration-none" >˚F</a>  </span>
+          
+            </div>
+        );
+    } else{
+        let fahrenhiet = (props.celsiuc*9/5)+32;
+        return(
+            <div>
+    <strong >{Math.round(fahrenhiet)}</strong>
+          <span className="units">
+             <a href="/" onClick={showCelsiuc} className="text-decoration-none" >
+                ℃</a>
+                  | ˚F
+                    </span>
+          
+            </div>
+        );  
+    }
+    
+}
